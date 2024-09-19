78813a3fd1d36d33b2fed83f0de2a1fd1926edec
Pedro <peci@itu.dk>
2024-09-19 12:43:09 +0200

    Overlay layer changes + scene cleanup

--- Assets/Scripts/OverlayController.cs
+++ Assets/Scripts/OverlayController.cs
@@ -6,9 +6,7 @@ using UnityEngine.UI;
 
 public class OverlayController : MonoBehaviour
 {
-    [SerializeField]
-    GameObject panel;   
-    Image img;
+    private SpriteRenderer overlay;
 
     [SerializeField]
     byte alpha = 0x80;
@@ -16,7 +14,7 @@ public class OverlayController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        img = panel.GetComponent<Image>();
+        overlay = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,20 +24,20 @@ public class OverlayController : MonoBehaviour
         switch (season.ToString())
         {
             case "Summer":
-                img.color = new Color32(0xF9, 0xD6, 0x2E, alpha);
+                overlay.color = new Color32(0xF9, 0xD6, 0x2E, alpha);
                 break;
             case "Winter":
-                img.color = new Color32(0x00, 0xE4, 0xFF, alpha);
+                overlay.color = new Color32(0x00, 0xE4, 0xFF, alpha);
                 break;
             case "Spring":
-                img.color = new Color32(0x5E, 0x8D, 0x5A, alpha);
+                overlay.color = new Color32(0x5E, 0x8D, 0x5A, alpha);
                 break;
             case "Autumn":
-                img.color = new Color32(0xF4, 0x7B, 0x20, alpha);
+                overlay.color = new Color32(0xF4, 0x7B, 0x20, alpha);
                 break;
             default:
                 break;
         }
-        Debug.Log(img.color);
+        Debug.Log(overlay.color);
     }
 }
